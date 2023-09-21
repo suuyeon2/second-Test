98433661e1ed4d880ef930fd9ad834bfc4e71e8a
jyllds22 <jyllds22@gmail.com>
2023-09-21 12:40:36 +0900

    Test1 sout 추가

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -3,5 +3,7 @@ package test;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("hi");
+        System.out.println("bye");
     }
 }
